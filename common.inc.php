1feb61b25eade387b44c58501730badf8c9a7bc1
markus-hsk <blackpanther.online@gmx.de>
2016-12-13 13:48:56 +0100

    Grundlagen-Datei aktualisiert

--- common.inc.php
+++ common.inc.php
@@ -1,4 +1,23 @@
 <?php
+// Wenn RelativePath nicht definiert wurde, funktioniert das Projekt nicht richtig.
+if(!defined('RelativePath'))
+{
+	trigger_error('Notwendige PHP-Konstante "RelativePath" wurde nicht definiert.', E_USER_ERROR);
+	exit;
+}
+
+// Prüfen ob Konfiguration erstellt wurde
+if(!file_exists(RelativePath . '/cfg/config.inc.php'))
+{
+	trigger_error('Die notwendige Konfigurationsdatei /cfg/config.inc.php wurde noch nicht angelegt.', E_USER_ERROR);
+	exit;
+}
+
+// Notwendige Initial-Includes
+require_once(RelativePath.'/cfg/config.inc.php');
+require_once(RelativePath.'/lib/utils.php');
+
+
 // Ausgabe komprimieren - Deaktivieren wenn Apache mod_deflate verwendet wird
 if(!defined('USE_GZHANDLER') || USE_GZHANDLER == false)
 	ob_start();
@@ -6,30 +25,21 @@ else
 	ob_start('ob_gzhandler');
 
 
-// Wenn RelativePath nicht definiert wurde, funktioniert das Projekt nicht richtig.
-if(!defined('RelativePath'))
-	trigger_error('Notwendige PHP-Konstante "RelativePath" wurde nicht definiert.', E_USER_ERROR);
-
-
-// Notwendige Includes
-require_once(RelativePath.'/cfg/database.php');
-require_once(RelativePath.'/lib/rb.php');
-require_once(RelativePath.'/lib/Skin.class.php');
-require_once(RelativePath.'/lib/utils.php');
-
-
-
-// Datenbank initialisieren
-R::setup('mysql:host='.DB_HOST.':'.DB_PORT.';dbname='.DB_DATABASE, DB_USER, DB_PASSWORD);
-
-R::query()
-
 // Wartungsmodus aktiv?
 if(defined('MAINTENANCE_MODE') && MAINTENANCE_MODE == true)
 {
 	http_response_code(503); // Service Unavailable
 	header('Content-Type: application/json');
-	echo 'Datenbank befindet sich aktuell im Wartungsmodus.';
+
+	echo json_encode(array(
+		'success' => false,
+		'rows' => false,
+		'total' => 0,
+		'error' => array(
+			'error_message' => 'Diese Seite befindet sich aktuell im Wartungsmodus. Bitte versuchen Sie es später erneut',
+			'error_code' => -1040
+		)
+	));
 	exit(0);
 }
 
@@ -37,44 +47,55 @@ if(defined('MAINTENANCE_MODE') && MAINTENANCE_MODE == true)
 // dynamische Konstanten aus Konfiguration ableiten
 
 
-// Datei-Berechtigungen beim Schreiben automatisch beschrÃ¤nken?!
+// Datei-Berechtigungen beim Schreiben automatisch beschränken?!
 umask(000);
 
 
 // richtige Zeitzone und Sprache des Systems setzen
-ini_set('date.timezone', 'Europe/Berlin');
-// @todo in MSM auskommentiert, sollten wir uns ansehen. Evtl sollten wir setLocale fÃ¼r bestimmte Dinge wie
+date_default_timezone_set('UTC');
+
+
 // Stringvergleiche und Numerische Werte auf standard/ englisch setzen um Seiteneffekte zu vermeiden
-// setlocale(LC_ALL, 'de_DE@euro', 'de_DE', 'de', 'ge');
+setlocale(LC_ALL, 'de_DE@euro', 'de_DE', 'de', 'ge');
 
 
 // Session starten
-// @todo in MSM auskommentiert
-// ini_set("session.cookie_lifetime", 3600*24*3);
+if(defined('SESSION_LIVETIME'))
+	ini_set("session.cookie_lifetime", SESSION_LIVETIME);
 session_start();
 
 
 // Errorhandling
-//error_reporting(ERROR_REPORTING);
+if(defined('ERROR_REPORTING'))
+	error_reporting(ERROR_REPORTING);
 
 
 // Errorhandler setzen
-if(!defined('USE_ERROR_HANDLER') || USE_ERROR_HANDLER == true)
+if(defined('USE_ERROR_HANDLER') && USE_ERROR_HANDLER == true)
 {
-	//set_error_handler('mOErrorHandler');
+	// @todo
+	//require_once(RelativePath . '/scripts/includes/error_handler.inc.php');
+	//set_error_handler('');
 }
 
 
 // Zeichensatz
-define('CHARSET', 'UTF-8');		// @TODO sollte in die Konfiguration ausgelagert werden
+define('CHARSET', 'UTF-8');
 ini_set('default_charset', CHARSET);
 
 
-// Wenn max_input_vars ausgeschÃ¶pft wird, dann Fehlermeldung erzeugen, da es sich sonst nur um ein Warning handelt
-if(ini_get('max_input_vars') > 0 && count($_POST) == ini_get('max_input_vars'))
+// Wenn max_input_vars ausgeschöpft wird, dann Fehlermeldung erzeugen, da es sich sonst nur um ein Warning handelt
+$max_input_vars = ini_get('max_input_vars');
+if($max_input_vars > 0 && count($_POST) >= $max_input_vars)
 {
-	trigger_error('Zulässige Anzahl an Formularparametern überschritten - max_input_vars: '.ini_get('max_input_vars'), E_USER_ERROR);
+	trigger_error('Zulässige Anzahl an Formularparametern überschritten - max_input_vars: '.$max_input_vars, E_USER_ERROR);
 }
+
+// Versionsnummer
+define('VERSION', '0.0.1');
+
+// Weitere Includes tätigen
+require_once(RelativePath.'/lib/Skin.class.php');
 
 
 ?>
