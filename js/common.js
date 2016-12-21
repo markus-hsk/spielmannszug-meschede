8020c35e7fd933f4f57b6d3c4126a71a7a059d4b
markus-hsk <blackpanther.online@gmx.de>
2016-12-21 13:48:47 +0100

    Mitgliederliste sortierbar machen

--- js/common.js
+++ js/common.js
@@ -38,6 +38,7 @@ function searchMembers()
 
 
 var members = null;
+var members_sortby = 'LASTNAME ASC';
 function getMembers()
 {
 	if(members === null)
@@ -46,23 +47,69 @@ function getMembers()
 		return;
 	}
 
+	// Einträge sortieren
+	sortMembers();
+
 	var output = '';
 	for(i = 0; i < members.length; i++)
 	{
 		var member = members[i];
 
+		var member_id = member.MEMBER_ID;
+		var lastname = member.LASTNAME;
+		var firstname = member.FIRSTNAME;
+		var birthname = member.BIRTHNAME;
+		var street = member.STREET;
+		var zip = member.ZIP;
+		var city = member.CITY;
+
+		if(member.GENDER == 'w')
+			var gendersign = '<i class="fa fa-venus female" aria-hidden="true" aria-label="weiblich"></i>';
+		else
+			var gendersign = '<i class="fa fa-mars male" aria-hidden="true" aria-label="männlich"></i>';
+
+		var birthdate = (member.BIRTHDATE != null && member.BIRTHDATE.length && member.BIRTHDATE != '0000-00-00') ? new Date(member.BIRTHDATE) : null;
+		var birthdate_output = birthdate != null ? birthdate.getDate() + '.' + (birthdate.getMonth()+1) + '.' + birthdate.getFullYear() : '<i>unbekannt</i>';
+
+		if(member.DEATHDATE == null || !member.DEATHDATE.length || member.DEATHDATE == '0000-00-00')
+		{
+			var age = birthdate != null ? '(' + getAge(birthdate, null) + ')' : '';
+			var deadsign = '';
+			var state = member.CURRENT_STATE;
+		}
+		else
+		{
+			var deathdate = new Date(member.DEATHDATE);
+			var deathdate_output = deathdate.getDate() + '.' + (deathdate.getMonth()+1) + '.' + deathdate.getFullYear();
+			var deadsign = ' <b>&dagger;</b>';
+
+			if(birthdate != null)
+			{
+				birthdate_output += ' -';
+				var age = deathdate_output + ' (' + getAge(birthdate, deathdate) + ')';
+			}
+			else
+			{
+				var age = '- ' + deathdate_output;
+			}
+
+			var state = 'verstorben';
+		}
+
+		var contact = '';
+
 		output += '<tr class="simple">' +
-						'<!-- <td>' + member.MEMBER_ID + '</td> -->' +
-						'<td data-label="Nachname" class="responsive_bolder">' + member.LASTNAME + member.DEAD + ' <br><span class="birthname">' + member.BIRTHNAME + '</span></td>' +
-						'<td data-label="Vorname">' + member.FIRSTNAME + '</td>' +
-						'<td data-label="Geschlecht" class="hideResponsiveSimple">' + member.GENDER + '</td>' +
+						'<!-- <td>' + member_id + '</td> -->' +
+						'<td data-label="Nachname" class="responsive_bolder">' + lastname + deadsign + ' <br><span class="birthname">' + birthname + '</span></td>' +
+						'<td data-label="Vorname">' + firstname + '</td>' +
+						'<td data-label="Geschlecht" class="hideResponsiveSimple">' + gendersign + '</td>' +
 						'<td data-label="Adresse" class="hideResponsiveSimple">' +
-							'<div>' + member.STREET + ' <br>' + member.ZIP + ' ' + member.CITY + '</div>' +
+							'<div>' + street + ' <br>' + zip + ' ' + city + '</div>' +
 						'</td>' +
-						'<td data-label="Geburtstag" class="hideResponsiveSimple">' + member.BIRTHDATE + ' <br>' + member.AGE + '</td>' +
-						'<td data-label="Status" class="hideResponsiveSimple">' + member.STATE + '</td>'+
+						'<td data-label="Geburtstag" class="hideResponsiveSimple">' + birthdate_output + ' <br>' + age + '</td>' +
+						'<td data-label="Status" class="hideResponsiveSimple">' + state + '</td>'+
 						'<td data-label="Kontaktdaten" class="hideResponsiveSimple">' +
-							'<div>' + member.CONTACT + '</div>' +
+							'<div>' + contact + '</div>' +
 						'</td>' +
 					'</tr>';
 	}
@@ -74,11 +121,99 @@ function getMembersFromServer()
 {
 	$.ajax({
 			   url:  'memberlist.php',
-			   data: {},
+			   data: {}
 		   }).done(function(data)
 							  {
 								  members = data;
 
 								  getMembers();
 							  });
+}
+
+function sortMembers()
+{
+	members.sort(function(a,b)
+				 {
+					 var sortby = members_sortby.split(' ');
+					 var result = 0;
+
+					 switch(sortby[0])
+					 {
+						 default:
+						 case 'LASTNAME':
+							 if(a.LASTNAME != b.LASTNAME)
+								 result = a.LASTNAME < b.LASTNAME ? -1 : 1;
+							 else
+								 result = a.FIRSTNAME < b.FIRSTNAME ? -1 : 1;
+							 break;
+
+						 case 'FIRSTNAME':
+							 if(a.FIRSTNAME != b.FIRSTNAME)
+								 result = a.FIRSTNAME < b.FIRSTNAME ? -1 : 1;
+							 else
+								 result = a.LASTNAME < b.LASTNAME ? -1 : 1;
+							 break;
+
+						 case 'GENDER':
+							 if(a.GENDER != b.GENDER)
+								 result = a.GENDER < b.GENDER ? -1 : 1;
+							 else
+								 result = 0;
+							 break;
+
+						 case 'BIRTHDATE':
+							 if(a.BIRTHDATE != b.BIRTHDATE)
+								 result = a.BIRTHDATE < b.BIRTHDATE ? -1 : 1;
+							 else
+								 result = 0;
+							 break;
+
+						 case 'STATE':
+							 if(a.CURRENT_STATE != b.CURRENT_STATE)
+								 result = a.CURRENT_STATE < b.CURRENT_STATE ? -1 : 1;
+							 else
+								 result = 0;
+							 break;
+					 }
+
+					 if(sortby[1] == 'DESC')
+					 	result = result * -1;
+
+					 // Sortierlink anpassen
+					 $('.sorter').removeClass('active_sorter');
+					 $('.sorter i.sortindicator').addClass('fa-sort');
+					 $('.sorter i.sortindicator').removeClass('fa-sort-asc');
+					 $('.sorter i.sortindicator').removeClass('fa-sort-desc');
+
+					 $('#SORTER_' + sortby[0]).addClass('active_sorter');
+
+					 if(sortby[1] == 'ASC')
+						 $('#SORTER_' + sortby[0] + ' i.sortindicator').addClass('fa-sort-asc');
+					 else
+						 $('#SORTER_' + sortby[0] + ' i.sortindicator').addClass('fa-sort-desc');
+
+					 return result;
+				 });
+}
+
+function membersSortBy(sortby)
+{
+	var old_sortby = members_sortby.split(' ');
+
+	if(old_sortby[0] == sortby && old_sortby[1] == 'ASC')
+		members_sortby = sortby + ' DESC';
+	else
+		members_sortby = sortby + ' ASC';
+
+	getMembers();
+}
+
+function getAge(birthdate, until)
+{
+	if(until == null)
+		until = new Date();
+
+	var ageDifMs = until.getTime() - birthdate.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
