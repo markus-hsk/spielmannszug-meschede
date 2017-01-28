18dd2b93c51694fd2116375883f3cbbe39674a8e
Markus B <blackpanther.online@gmx.de>
2017-01-28 12:35:10 +0100

    Aufräumen

--- content.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('RelativePath', '.');
-require_once(RelativePath.'/common.inc.php');
-
-$Skin = new Skin(RelativePath.'/templates/members.html');
-echo $Skin;
--- index_alt.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('RelativePath', '.');
-require_once(RelativePath.'/common.inc.php');
-
-
-$Skin = new Skin(RelativePath.'/templates/template.html');
-echo $Skin;
