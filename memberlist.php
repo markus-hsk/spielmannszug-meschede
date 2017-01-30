a5e1a4e6ec7031632b8ab695dbfe26dd824862e7
Markus B <blackpanther.online@gmx.de>
2017-01-30 18:40:50 +0100

    gesamte aufbereitete Mitgliederliste aus dem Cache laden

--- memberlist.php
+++ memberlist.php
@@ -9,13 +9,20 @@
 define('RelativePath', '.');
 require_once(RelativePath.'/common.inc.php');
 
-
-$members = Member::find([]);
-
-$records = array();
-foreach ($members as &$Member)
+$cachekey = 'memberlist';	// @todo Zeitstempel aus Member-Klasse holen, um bei Veralterung der Daten nicht mehr auf diesen zurück zu greifen.
+							// alternativ diesen Cache beim ändern eines Users automatisch löschen
+$records = Cache::get($cachekey);
+if($records === false)
 {
-	$records[] = $Member->getDataArrayDeep();
+	$members = Member::find([]);
+	
+	$records = array();
+	foreach ($members as &$Member)
+	{
+		$records[] = $Member->getDataArrayDeep();
+	}
+	
+	Cache::set($cachekey, $records);
 }
 
 
