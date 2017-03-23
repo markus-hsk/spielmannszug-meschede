c13276da157e3d102808006d2606466559ca1d0e
Markus B <blackpanther.online@gmx.de>
2017-03-23 17:09:34 +0100

    [ADD] Default-Daten bei SQLs ebenfalls anlegen

--- setup/database.sql
+++ setup/database.sql
@@ -32,3 +32,16 @@ CREATE TABLE IF NOT EXISTS `spz_membership_states` (
   `END_DATE` date DEFAULT NULL,
   PRIMARY KEY (`MEMBERSHIP_ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Diese Tabelle speichert die Statuswechsel eines Mitglieds';
+
+
+/* Default-Daten: */
+INSERT IGNORE INTO `spz_members` (`MEMBER_ID`, `LASTNAME`, `FIRSTNAME`, `BIRTHNAME`, `GENDER`, `STREET`, `ZIP`, `CITY`, `BIRTHDATE`, `DEATHDATE`, `INSTRUMENT`) VALUES
+(1, 'Buscher', 'Markus', '', 'm', 'Von Westfalen-Str. 29', '59872', 'Meschede', '1985-11-19', NULL, 'Trommel');
+
+INSERT IGNORE INTO `spz_contact_informations` (`CONTACT_ID`, `MEMBER_ID`, `CONTACT_TYPE`, `VALUE`) VALUES
+(1, 1, 'email', 'markus@buscher.de'),
+(2, 1, 'mobile', '0175/1515084'),
+(3, 1, 'phone', '0291/14497586');
+
+INSERT IGNORE INTO `spz_membership_states` (`MEMBERSHIP_ID`, `MEMBER_ID`, `STATE`, `START_DATE`, `END_DATE`) VALUES
+(1, 1, 'aktiv', '1998-01-01', NULL);
