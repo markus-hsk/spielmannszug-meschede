bb169c772fdfce2a19f9bab4efff461d9c4bb74d
Markus B <blackpanther.online@gmx.de>
2019-01-16 16:33:39 +0100

    Paging für Mitgliederliste implementiert #11

--- js/controllers/memberListController.js
+++ js/controllers/memberListController.js
@@ -27,6 +27,10 @@ angular.module('spzdb')
 			   me.filters.age_child          = 1;
                me.filters.instrument		 = 'all';
 
+               me.page = 1;
+               me.max_rows = 30;
+               me.max_pages = 1;
+               me.all = [];
 			   me.rows = [];
 
 			   me.load = function()
@@ -41,12 +45,83 @@ angular.module('spzdb')
 										  $("#mainview").show();
 										  $("#loader").hide();
 
-										  me.rows = memberService.getList(me.getFilters(), me.getSortBy());
-
-										  $('#footer_container')
-											  .html('Anzahl Mitglieder: ' + me.rows.length + ' von ' + memberService.getTotalUnfiltered());
+										  me.all = memberService.getList(me.getFilters(), me.getSortBy());
+										  
+										  if(me.all.length > me.max_rows)
+										  {
+											  me.max_pages = Math.ceil(me.all.length / me.max_rows);
+											  page = Math.min(me.page, me.max_pages);
+											  
+											  me.showPage(page);
+										  }
+										  else
+										  {
+											  me.max_pages = 1;
+											  me.showPage(1);
+										  }
 									  }
 				   );
+			   };
+			   
+			   me.showPage = function(page)
+			   {
+				   debugSpzDb('memberListController->showPage() Call', page);
+
+				   me.page = page;
+				   me.rows = [];
+				   
+				   offset = me.max_rows * (me.page - 1);
+				   for(i = 0; i < me.max_rows; i++)
+				   {
+					   if(me.all.length <= offset + i)
+					   {
+						   break;
+					   }
+					   
+					   record = me.all[offset + i];
+					   record.index = offset + i + 1;
+					   me.rows.push(record);
+				   }
+			   };
+			   
+			   me.firstPage = function()
+			   {
+				   debugSpzDb('memberListController->firstPage() Call');
+
+				   if(me.page > 1)
+				   {
+					   me.showPage(1);
+				   }
+			   };
+			   
+			   me.lastPage = function()
+			   {
+				   debugSpzDb('memberListController->lastPage() Call');
+
+				   if(me.page < me.max_pages)
+				   {
+					   me.showPage(me.max_pages);
+				   }
+			   };
+			   
+			   me.previousPage = function()
+			   {
+				   debugSpzDb('memberListController->previousPage() Call');
+
+				   if(me.page > 1)
+				   {
+					   me.showPage(me.page - 1);
+				   }
+			   };
+			   
+			   me.nextPage = function()
+			   {
+				   debugSpzDb('memberListController->nextPage() Call');
+
+				   if(me.page < me.max_pages)
+				   {
+					   me.showPage(me.page + 1);
+				   }
 			   };
 
 			   me.sortBy = function(field)
@@ -66,13 +141,11 @@ angular.module('spzdb')
 					   me.current_sort_dir   = 'asc';
 				   }
 
-				   //me.rows = memberService.getList(me.getFilters(), me.getSortBy());
 				   me.load();
 			   };
 
 			   me.filterList = function()
 			   {
-				   //me.rows = memberService.getList(me.getFilters(), me.getSortBy());
 				   me.load();
 			   };
 
